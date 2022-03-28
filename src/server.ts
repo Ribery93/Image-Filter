099325eb8320570621b93cc37be19c447cf1d617
Arthur  Silva <arthur.silva@quikdev.com.br>
2022-03-28 09:21:25 -0300

    change code

--- src/server.ts
+++ src/server.ts
@@ -29,16 +29,17 @@ import {constants} from "http2";
 
   /**************************************************************************** */
 
-  app.get( "/filteredimage", async ( req, res ) => {
+  app.get( "/filteredimage", async (req:express.Request, res:express.Response) => {
     let {image_url} = req.query;
     if (!image_url){
-      res.status(constants.HTTP_STATUS_BAD_REQUEST).send('Error : Empty image url submitted');
+      res.status(constants.HTTP_STATUS_UNSUPPORTED_MEDIA_TYPE).send('Error : Empty image url submitted');
     } else {
       await filterImageFromURL(image_url).then( function (filtered_path){
         res.sendFile(filtered_path, () => {
           deleteLocalFiles([filtered_path]);
         });
-      }).catch(()=> res.status(constants.HTTP_STATUS_BAD_REQUEST).send('The image can not be filtered - check the link submitted '));
+        res.status(constants.HTTP_STATUS_OK).send('Success : The image url was submitted')
+      }).catch(()=> res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send('The image can not be filtered - check the link submitted '));
     }
   });
 
